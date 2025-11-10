198a9260a85d627436e6e6da420c1a7bb400ab00
Rishig883 <137030870+Rishig883@users.noreply.github.com>
2025-11-11 02:32:16 +0530

    Update

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -79,10 +79,8 @@ export default function Login() {
             </div>
           </form>
           <div className="m-1.5 rounded-lg bg-gray-50 py-4 text-center text-sm/5 ring-1 ring-black/5">
-            Not a member?{' '}
-            <Link href="#" className="font-medium hover:text-gray-600">
-              Create an account
-            </Link>
+            {/* Not a member?{' '} */}
+            {/* <Link href="#" className="font-medium hover:text-gray-600"></Link> */}
           </div>
         </div>
       </div>
